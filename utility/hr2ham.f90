fd96d39e7db43e5a1a367ce927ef11188acc7783
Xiaoming Wang <40604687+GnawX@users.noreply.github.com>
2023-07-03 17:31:52 -0400

    Update hr2ham.f90

--- utility/hr2ham.f90
+++ utility/hr2ham.f90
@@ -2,13 +2,20 @@ PROGRAM hr2ham
 !
 ! convert wannier90_hr.dat file to WanT ham file that
 ! can be directly used by conductor.x to calculated transmission
+! compile with "gfortran -o write_ham.x write_ham.f90 -I ../extlibs/iotk/include -L ../extlibs/iotk/lib -liotk"
+
+  USE iotk_module
+!
+  IMPLICIT NONE
+
 
   DP = KIND(1.0d0)
   
-  INTEGER            :: i, j, ir, nrtot, file_unit
-  CHARACTER(len=33)  :: header
+  INTEGER            :: i, j, ir, nrtot, file_unit, dimwann, stdout, &
+                        nsize
+  CHARACTER(len=33)  :: header, fileout
 
-  INTEGER,     ALLOCATABLE :: ivr(:,:)
+  INTEGER,     ALLOCATABLE :: ivr(:,:), iwr(:)
   REAL(dp),    ALLOCATABLE :: wr(:)
   COMPLEX(dp), ALLOCATABLE :: rham(:,:,:)
 
@@ -18,18 +25,59 @@ PROGRAM hr2ham
           status='unknown', err=101)
 
   READ (file_unit, *) header ! Date and time
-write (file_unit, *) num_wann
-write (file_unit, *) nrpts
-write (file_unit, '(15I5)') (ndegen(i), i=1, nrpts)
-do irpt = 1, nrpts
-do i = 1, num_wann
-do j = 1, num_wann
-      write (file_unit, '(5I5,2F12.6)') irvec(:, irpt), j, i, &
-         ham_r(j, i, irpt)
-    end do
-  end do
-end do
+  READ (file_unit, *) dimwann
+  READ (file_unit, *) nrtot
 
-close (file_unit)
+  ALLOCATE (ivr(3,nrtot), wr(nrtot), iwr(nrtot), rham(dimwann,dimwann,nrtot))
+  
+  READ (file_unit, '(15I5)') (iwr(i), i=1, nrtot)
+  DO ir = 1, nrtot
+    DO i = 1, dimwann
+      DO j = 1, dimwann
+        READ (file_unit, '(5I5,2F12.6)') ivr(:, ir), j, i, &
+              rham(j, i, ir)
+      END DO
+    END DO
+  END DO
+
+  CLOSE (file_unit)
+
+  wr = iwr/SUM(iwr)
+
+! write ham file
+
+  stdout = 222
+  fileout = 'wannier90.ham'
+  CALL iotk_open_write( stdout, FILE=TRIM(fileout))
+
+  CALL iotk_write_begin(stdout,"HAMILTONIAN")
+
+  CALL iotk_write_attr( attr, "dimwann", dimwann, FIRST=.TRUE. )
+  CALL iotk_write_attr( attr, "nrtot", nrtot )
+  !CALL iotk_write_attr( attr, "nr", nr )
+  !CALL iotk_write_attr( attr, "have_overlap", have_overlap )
+  !CALL iotk_write_attr( attr, "fermi_energy", fermi_energy )
+
+  CALL iotk_write_empty( stdout, "DATA", ATTR=attr)
+
+  nsize=3*nrtot
+  CALL iotk_write_attr( attr, "type", "integer", FIRST=.TRUE. )
+  CALL iotk_write_attr( attr, "size", nsize )
+  CALL iotk_write_attr( attr, "columns", 3 )
+  CALL iotk_write_attr( attr, "units", "crystal" )
+  CALL iotk_write_dat( stdout, "IVR", ivr, COLUMNS=3, ATTR=attr )
+
+  CALL iotk_write_attr( attr, "type", "real", FIRST=.TRUE. )
+  CALL iotk_write_attr( attr, "size", nkpts )
+  CALL iotk_write_dat( stdout, "WR", wr, ATTR=attr )
+
+  CALL iotk_write_begin(stdout,"RHAM")
+  DO ir = 1, nrtot
+      CALL iotk_write_dat(stdout,"VR"//TRIM(iotk_index(ir)), rham(:,:,ir))
+  END DO
+  CALL iotk_write_end(stdout,"RHAM")
+  CALL iotk_write_end(stdout,"HAMILTONIAN")
+
+  CALL iotk_close_write( stdout )
 
 END PROGRAM hr2ham
